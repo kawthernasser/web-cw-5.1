119c033169cb8ed7d82be84adb269136aec7f61d
kawthernasser <66731849+kawthernasser@users.noreply.github.com>
2020-06-28 00:18:59 +0300

    done tho i think i made a mistake

---
+++ script.js
@@ -0,0 +1,17 @@
+const name="kawther";
+let counter= 16;
+counter= i+""+am+""+kawther+""+and+""+my+""+age+""+is+""+16;
+counter=counter-3;
+console.log(counter);
+counter=conter*5;
+console.log(counter);
+const codded;{
+name:"codded",
+path:wordpress
+teacher:msmoudhi 
+},
+const age ={16,16};
+console.log(age);
+food.push(16);
+console.log(age);
+
