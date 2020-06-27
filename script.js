326da88c75c851384fd92562e987536b6a2d1f6a
kawthernasser <66731849+kawthernasser@users.noreply.github.com>
2020-06-28 01:27:43 +0300

    DOOOONE

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const name="kawther";
+const name="kawther"
 let counter= 16;
-counter= i+""+am+""+kawther+""+and+""+my+""+age+""+is+""+16;
-counter=counter-3;
-console.log(counter);
-counter=conter*5;
-console.log(counter);
-const codded;{
-name:"codded",
-path:wordpress
-teacher:msmoudhi 
-},
-const age ={16,16};
-console.log(age);
-food.push(16);
-console.log(age);
+console.log("iam"+ name+ " " +"my age is"+ counter)
 
+console.log(counter*5);
+
+let hobbies=[
+    "drawing","shooting","swimming"
+,"16"]
+hobbies.push(counter)
+hobbies.pop()
+console.log(hobbies)
+
+let web={
+    path:"web",
+    lang:"html",
+    names:["monawer","moudhi","rahaf","hussain","nancy"]
+}
+console.log(web.path)
+console.log(web.names[1])
